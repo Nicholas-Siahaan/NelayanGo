35c85c7d1610e39a61683f42e667c02d2c739e4d
Nicholas-Siahaan <nicholasshanepangihutansiahaan@mail.ugm.ac.id>
2025-11-27 01:07:47 +0700

    sebagian profil

--- Nelayan.cs
+++ Nelayan.cs
@@ -9,6 +9,7 @@ namespace NelayanGo.Models
         public string Nama { get; set; } = string.Empty;
         public string KodeIdentik { get; set; } = string.Empty;
         public string Wilayah { get; set; } = string.Empty;
+        public string TahunBergabung { get; set; } = string.Empty;
         public string Status { get; set; } = string.Empty;
         public string NIK { get; set; } = string.Empty;
         public string TanggalLahir { get; set; } = string.Empty;
@@ -17,6 +18,7 @@ namespace NelayanGo.Models
         public string Email { get; set; } = string.Empty;
         public string Agama { get; set; } = string.Empty;
         public string GolonganDarah { get; set; } = string.Empty;
+        public string AlamatKTP { get; set; } = string.Empty;
 
         // Data Info
         public int TotalWaktuAktifHari { get; set; }
